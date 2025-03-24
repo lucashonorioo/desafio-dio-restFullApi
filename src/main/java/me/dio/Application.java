a642113627508e082571d267c4e91325e8c26a11
Lucas Honorio <lucashonorioo@hotmail.com>
2025-03-24 15:23:08 -0300

    Correção precision e scale

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setRegisterShutdownHook(true);
+		app.run(args);
 	}
-
 }
