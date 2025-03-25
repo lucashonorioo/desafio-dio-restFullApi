06da98f493772f7e55d810de6cb8695d447cf97c
Lucas Honorio <lucashonorioo@hotmail.com>
2025-03-25 16:43:51 -0300

    modificando OpenAPIDefinition

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,11 +1,14 @@
 package me.dio;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = { @Server(url = "/") })
+@OpenAPIDefinition(
+		info = @Info(title = "API Santander Dev Week", version = "1.0", description = "Documentação da API")
+)
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
